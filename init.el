b4e7a902ed8ea0e00d52f963e9274e7a818c5ac0
__ibrahim__ <ibrahim.hindawi@robocomvr.com>
2022-11-06 15:02:13 +0400

    updated init.el

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
 ;; Init default settings
-(load-theme 'wombat)
+(load-theme 'modus-vivendi)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (tooltip-mode -1)
@@ -8,7 +8,8 @@
 (set-default 'truncate-lines t)
 (set-face-attribute 'default nil :font "Consolas" :height 128)
 (windmove-default-keybindings)
-(global-hl-line-mode t)
+(global-hl-line-mode 1)
+(set-face-foreground 'highlight nil)
 
 ;; (customize-option global-superword-mode)
 ;; (customize-option global-subword-mode)
@@ -17,7 +18,7 @@
 ;; (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 (global-set-key (kbd "C-S-d") 'c-electric-delete)
 (global-set-key (kbd "M-o") 'other-window)
-
+(global-set-key (kbd "C-M-d") 'c-electric-backspace)
 ;; (global-set-key (kbd "C-l") 'forward-char)
 ;; (global-set-key (kbd "C-j") 'backward-char)
 ;; (global-set-key (kbd "M-l") 'forward-to-word)
@@ -49,7 +50,6 @@
 (setq use-package-always-ensure t)
 (use-package command-log-mode)
 
-;; Download Ivy
 (use-package ivy
   :diminish
   :bind (("C-s" . swiper)
@@ -68,7 +68,6 @@
   :config
   (ivy-mode 1))
 
-;; Download Counsel
 (use-package counsel
   :bind (("M-x" . counsel-M-x)
 	 ("C-x b" . counsel-ibuffer)
@@ -78,42 +77,39 @@
   :config
   (setq ivy-initial-inputs-alist nil))
 
-;; Doom Modeline
 (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode 1)
   :custom (doom-modeline-height 15))
 
-;; Doom themes
-(use-package doom-themes
-  :ensure t
+(use-package company
   :config
-  ;; Global settings (defaults)
-  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-        doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-one t)
+  (setq company-minimum-prefix-length 1
+	company-idle-delay 0.0
+	global-company-mode 1)) ; default is 2.0
+(require 'company)
 
-  ;; Enable flashing mode-line on errors
-  (doom-themes-visual-bell-config)
-  ;; Enable custom neotree theme (all-the-icons must be installed!)
-  (doom-themes-neotree-config)
-  ;; or for treemacs users
-  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
-  (doom-themes-treemacs-config)
-  ;; Corrects (and improves) org-mode's native fontification.
-  (doom-themes-org-config))
-
-;; Evil Mode
 (unless (package-installed-p 'evil)
   (package-install 'evil))
-;; Enable Evil
 (require 'evil)
 (evil-mode 0)
 
-;; which key
 (use-package which-key)
 (require 'which-key)
 (which-key-mode)
+
+;; (use-package lsp-mode
+  ;; :commands (lsp-mode-deferred)
+  ;; :init
+  ;; (setq lsp-keymap-prefix "C-c l")
+  ;; :config
+  ;; (lsp-enable-which-key-integration t))
+
+(use-package eglot)
+(require 'eglot)
+(add-to-list 'eglot-server-programs '((c++-mode c-mode) "clangd"))
+(add-hook 'c-mode-hook 'eglot-ensure)
+(add-hook 'c++-mode-hook 'eglot-ensure)
 
 ;; Custom vars
 (custom-set-variables
@@ -122,7 +118,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(which-key evil doom-modeline counsel ivy command-log-mode use-package)))
+   '(company which-key evil doom-modeline counsel ivy command-log-mode use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -132,7 +128,7 @@
  '(cursor ((t (:background "dark violet"))))
  '(font-lock-builtin-face ((t (:foreground "orange"))))
  '(font-lock-constant-face ((t (:foreground "orange"))))
- '(font-lock-function-name-face ((t (:foreground "burlywood1"))))
+ '(font-lock-function-name-face ((t (:foreground "orange"))))
  '(font-lock-keyword-face ((t (:foreground "orange" :weight normal))))
  '(font-lock-preprocessor-face ((t (:inherit normal :foreground "burlywood1"))))
  '(font-lock-string-face ((t (:foreground "DarkOrange1"))))
